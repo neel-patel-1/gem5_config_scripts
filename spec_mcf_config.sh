f622446f888ab2ced8d5a041714257083f4969f8
Neel Patel <n869p538@pollux.ittc.ku.edu>
2022-11-25 13:01:39 -0600

    longer simtick duration

--- spec_mcf_config.sh
+++ spec_mcf_config.sh
@@ -2,5 +2,6 @@
 export BIN=/home/n869p538/wrk_offloadenginesupport/async_nginx_build/cpu_2017/benchspec/CPU/505.mcf_r/build/build_base_mytest-m64.0000/mcf_r
 export ARGS="/home/n869p538/wrk_offloadenginesupport/async_nginx_build/cpu_2017/benchspec/CPU/505.mcf_r/run/run_base_refrate_mytest-m64.0000/inp.in"
 export OUTDIR=spec_mcf
-export SIM_TICKS=50500000000 # 1m20s for MCF_R default
+#export SIM_TICKS=50500000000 # 1m20s for MCF_R default
+export SIM_TICKS=$(( 50500000000 * 45 ))  # 1h for MCF_R default
 
