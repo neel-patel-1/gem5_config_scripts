96de777cbe0333ebe59a17a1fb7a91797fbcb4e2
Neel Patel <n869p538@pollux.ittc.ku.edu>
2022-11-25 11:00:13 -0600

    generic scripts using config files

---
+++ atomic_timeout_memmax10MB.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+GEM5_DIR=$(pwd)/../gem5
+GEM5_EXE=$GEM5_DIR/build/X86/gem5.opt
+
+SE_PATH=/opt/shared/gem5-learning/gem5/configs/example/se.py
+CheckPoint=$(pwd)/spec_mcf_r_test
+
+[ -z "$1" ] && echo "No Source Config File Provided" && exit -1
+source ./default_config.sh
+[ -z "$OUTDIR" ] && echo "No OUTPUT DIRECTORY Provided" && exit -1
+OUTDIR=${OUTDIR}_memmax10MB
+[ -z "$BIN" ] && echo "No Binary Provided" && exit -1
+[ -z "$ARGS" ] && echo "No Binary ARGUMENTS" && exit -1
+
+# set core 5 to use last llc way
+sudo pqos -R 
+sudo pqos -e 'mba_max:1=10;'
+sudo pqos -a 'cos:1=5'
+
+#BENCHMARK
+
+
+taskset -c 5 $GEM5_EXE --outdir=${OUTDIR} $SE_PATH 	\
+                    --cpu-type=AtomicSimpleCPU	\
+                    --num-cpus=4               \
+					--mem-channels=1			\
+					--cpu-clock=4GHz			    \
+					--sys-clock=4GHz			    \
+					--mem-type=DDR3_1600_8x8	\
+					--mem-channels=1		    \
+					--mem-size=2GB				\
+					--caches					\
+					--l1i_size=32kB				\
+					--l1i_assoc=2				\
+					--l1d_size=64kB				\
+					--l1d_assoc=2				\
+					--l2_size=2MB				\
+					--l2_assoc=16				\
+					--bp-type=BiModeBP			\
+					--bp-type=BiModeBP			\
+					--checkpoint-dir=$CheckPoint \
+					--cmd=${BIN}			\
+					--rel-max-tick=50500000000  \
+					--options="${ARGS}"
