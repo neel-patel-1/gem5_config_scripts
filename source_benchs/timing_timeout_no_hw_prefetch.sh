2a884f283cafd15594d238f182515e7715591ffe
Neel Patel <n869p538@pollux.ittc.ku.edu>
2022-11-26 17:41:14 -0600

    timing cpu scripts

---
+++ source_benchs/timing_timeout_no_hw_prefetch.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+GEM5_DIR=$(pwd)/../gem5
+GEM5_EXE=$GEM5_DIR/build/X86/gem5.opt
+
+SE_PATH=/opt/shared/gem5-learning/gem5/configs/example/se.py
+CheckPoint=$(pwd)/spec_mcf_r_test
+
+[ -z "$1" ] && echo "No Source Config File Provided" && exit -1
+source ./default_config.sh
+source ${1}
+[ -z "$OUTDIR" ] && echo "No OUTPUT DIRECTORY Provided" && exit -1
+OUTDIR=${OUTDIR}_no_hw_preftch
+[ -z "$BIN" ] && echo "No Binary Provided" && exit -1
+[ -z "$SIM_TICKS" ] && echo "No SIM_TICKS SPECIFIED" && exit -1 
+OUTDIR=${OUTDIR}_${SIM_TICKS}_simticks_timing
+[ -z "$ARGS" ] && echo "No Binary ARGUMENTS" && exit -1
+#BENCHMARK
+
+
+sudo wrmsr -a 0x1a4 15
+
+
+taskset -c 5 $GEM5_EXE --outdir=${OUTDIR} $SE_PATH 	\
+                    --cpu-type=TimingSimpleCPU	\
+                    --num-cpus=4               \
+					--mem-channels=1			\
+					--cpu-clock=4GHz			    \
+					--sys-clock=4GHz			    \
+					--mem-type=DDR3_1600_8x8	\
+					--mem-channels=1		    \
+					--mem-size=2GB				\
+					--caches					\
+					--l1i_size=32kB				\
+					--l1i_assoc=2				\
+					--l1d_size=64kB				\
+					--l1d_assoc=2				\
+					--l2_size=2MB				\
+					--l2_assoc=16				\
+					--bp-type=BiModeBP			\
+					--bp-type=BiModeBP			\
+					--checkpoint-dir=$CheckPoint \
+					--cmd=${BIN}			\
+					--rel-max-tick=${SIM_TICKS}  \
+					--options="${ARGS}"
+echo "output directory:${OUTDIR}"
