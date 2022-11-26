3fa808a600e91aa040149d13d1b0d7cc57168136
Neel Patel <n869p538@castor.ittc.ku.edu>
2022-11-25 19:11:16 -0600

    longer parsec streamcluster

--- parsec_streamcluster_config.sh
+++ parsec_streamcluster_config.sh
@@ -1,7 +1,7 @@
 
 export BIN=/opt/shared/parsec_native/parsec-3.0/pkgs/kernels/streamcluster/inst/amd64-linux.gcc/bin/streamcluster
-export ARGS="2 5 1 10 10 5 none output.txt 1"
-export OUTDIR=parsec_streamcluster
+export ARGS="10 20 128 16384 16384 1000 none output.txt 4"
+export OUTDIR=parsec_streamcluster_largeinput_4tds
 #export SIM_TICKS=50500000000 # 1m20s for MCF_R default
-export SIM_TICKS=$(( 50500000000 * 45 ))  # 1h for MCF_R default
+export SIM_TICKS=$(( 50500000000 * 45 ))  # spec_mcf derived 1H host length
 
