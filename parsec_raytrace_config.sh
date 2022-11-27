d1ae20d2e9e50bebd6828819d75ceec78cb8f0d7
Neel Patel <n869p538@pollux.ittc.ku.edu>
2022-11-26 20:32:43 -0600

    run_all checks

---
+++ parsec_raytrace_config.sh
@@ -0,0 +1,8 @@
+
+export BIN=/opt/shared/parsec_native/parsec-3.0/pkgs/apps/raytrace/inst/amd64-linux.gcc/bin/rtview
+export ARGS="/opt/shared/parsec_native/parsec-3.0/pkgs/apps/raytrace/run/happy_buddha.obj -automove -nthreads 1 -frames 3 -res 1920 1080"
+export OUTDIR=parsec_raytrace
+#export SIM_TICKS=50500000000 # 1m20s for MCF_R default
+export SIM_TICKS=$(( 50500000000 * 45 ))  # spec_mcf derived 1H host length
+#export SIM_TICKS=$(( 100 ))  # spec_mcf derived 1H host length
+
--- run_all_bench.sh
+++ run_all_bench.sh
@@ -4,7 +4,9 @@
 log=${1}_log
 
 echo -n "" > $log
-for i in source_benchs/*; do
+for i in runnable/*; do
+	yes | cp source_benchs/$(basename ${i}) ${i}
+	yes | cp ${i} ./
 	echo "./$(basename ${i}) ${1} "  | tee -a $log 
 	./$(basename ${i}) ${1} | tee -a $log
 done
