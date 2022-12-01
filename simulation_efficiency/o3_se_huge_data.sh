d62e8cf917813266b028b10600c0cac88a93cb64
Neel Patel <n869p538@pollux.ittc.ku.edu>
2022-12-01 10:21:18 -0600

    huge text

---
+++ simulation_efficiency/o3_se_huge_data.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+GEM5_DIR=$(pwd)/../gem5
+GEM5_EXE=$GEM5_DIR/build/X86/gem5.opt
+
+SE_PATH=/opt/shared/gem5-learning/gem5/configs/example/se.py
+
+export VTUNE_WAIT=$(( 60  ))
+export MON_DELAY=$(( 10 ))
+
+[ -z "$1" ] && echo "No Source Config File Provided" && exit -1
+source ./default_config.sh
+source ${1}
+[ -z "$OUTDIR" ] && echo "No OUTPUT DIRECTORY Provided" && exit -1
+OUTDIR=simeff/${OUTDIR}_simeff_huge_data
+CheckPoint=${RESTORE_CPOINT}/
+[ -z "$BIN" ] && echo "No Binary Provided" && exit -1
+[ -z "$SIM_TICKS" ] && echo "No SIM_TICKS SPECIFIED" && exit -1 
+[ -z "$ARGS" ] && echo "No Binary ARGUMENTS" && exit -1
+#BENCHMARK
+
+echo "0" |  tee /sys/devices/system/cpu/intel_pstate/no_turbo
+
+LD_PRELOAD=libhugetlbfs.so \
+LD_LIBRARY_PATH=/usr/local/lib/lib64/:$LD_LIBRARY_PATH \
+HUGETLB_MORECORE=yes \
+taskset -c 5 $GEM5_EXE --outdir=${OUTDIR} $SE_PATH 	\
+                    --cpu-type=O3CPU	\
+                    --num-cpus=4               \
+					--mem-channels=1			\
+					--cpu-clock=4GHz			    \
+					--sys-clock=4GHz			    \
+					--mem-type=DDR3_1600_8x8	\
+					--mem-channels=1		    \
+					--mem-size=2GB				\
+					--caches					\
+					--l1i_size=32kB				\
+					--l1i_assoc=2				\
+					--l1d_size=64kB				\
+					--l1d_assoc=2				\
+					--l2_size=2MB				\
+					--l2_assoc=16				\
+					--bp-type=BiModeBP			\
+					--bp-type=BiModeBP			\
+					--checkpoint-dir=${CheckPoint} \
+					--checkpoint-restore=3 \
+					--cmd=${BIN}			\
+					--rel-max-tick=${SIM_TICKS}  \
+					--options="${ARGS}"
+
+
+echo "output directory:${OUTDIR}" 
